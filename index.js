1c1bb19b5ad7a089a68bf0b5f4f5100c66bb0503
parate362 <144500446+parate362@users.noreply.github.com>
2024-08-12 15:54:24 +0530

    update cors origin

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["http://localhost:5173","https://keek-client.vercel.app"];
 
 app.use(cors({
   origin: function (origin, callback) {
